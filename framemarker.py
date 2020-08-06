9f0970cb1365e5b975728062ac1b5da1c44dad7c
Admin <147523689a@gmail.com>
2020-08-06 13:49:46 +0700

    format code

--- framemarker.py
+++ framemarker.py
@@ -71,7 +71,6 @@ def process(frame):
                                       thresholdType=cv2.THRESH_BINARY_INV,
                                       blockSize=29, C=11)
 
-    # cv2.imshow("sađ", threshold)
     threshold = cv2.medianBlur(src=threshold, ksize=5)
 
     edges = cv2.Canny(image=threshold,
@@ -82,55 +81,56 @@ def process(frame):
                                            mode=cv2.RETR_TREE,
                                            method=cv2.CHAIN_APPROX_NONE)
 
-    contours = [contours[i] for i in range(len(contours)) if cv2.contourArea(contours[i]) > 3000.0]
+    contours = [contour for contour in contours if cv2.contourArea(contour) > 3000.0]
 
     list_approxs = []
-    for i in range(len(contours)):
+    for i, contour in enumerate(contours):
         epsilon = 0.05 * cv2.arcLength(contours[i], True)
         approx = cv2.approxPolyDP(contours[i], epsilon, True)
         hull = cv2.convexHull(approx)
 
         if len(hull) == 4 and (cv2.contourArea(hull) - cv2.contourArea(approx) <= 300.0):
             points = []
+            for _contour in hull:
+                points.append(list(_contour[0]))
 
-            for cnt in hull:
-                points.append(list(cnt[0]))
-
-            avg_length = (EuclidDistance(points[0], points[1]) + EuclidDistance(points[1], points[2]) +
-                          EuclidDistance(points[2], points[3]) + EuclidDistance(points[0], points[3])) / 4
+            avg_length = (EuclidDistance(points[0], points[1])
+                          + EuclidDistance(points[1], points[2])
+                          + EuclidDistance(points[2], points[3])
+                          + EuclidDistance(points[0], points[3])
+                          ) / 4
             min_threshold, max_threshold = avg_length * 0.8, avg_length * 1.2
             square_edges = [EuclidDistance(points[0], points[1]), EuclidDistance(points[1], points[2]),
                             EuclidDistance(points[2], points[3]), EuclidDistance(points[0], points[3])]
-
             bool_edges = np.logical_not(np.bitwise_and(min_threshold < np.array(square_edges),
                                                        np.array(square_edges) < max_threshold))
-            fl = not (np.any(bool_edges))
+            fl = not(np.any(bool_edges))
 
-            if fl is False:
-                continue
+            if fl:
+                list_approxs.append((hull, points))
 
-            list_approxs.append((hull, points))
+            del points
+        del hull
             # cv2.drawContours(frame, [hull], -1, (255, 0, 0), 3, 8)
 
     warped_images = []
 
-    for _ in list_approxs:
-        contour, points = _
-        pts = np.float32(points)
-        # print(pts)
-        dst_pts = np.float32(
+    for contour, points in list_approxs:
+        points = np.float32(points)
+
+        warped_points = np.float32(
             [[MARKER_RESIZE, 0],
              [MARKER_RESIZE, MARKER_RESIZE],
              [0, MARKER_RESIZE],
              [0, 0]]
         )
-        M = cv2.getPerspectiveTransform(pts, dst_pts)
+        M = cv2.getPerspectiveTransform(points, warped_points)
         warped = cv2.warpPerspective(src=frame, M=M,
                                      dsize=(MARKER_RESIZE, MARKER_RESIZE),
                                      flags=cv2.INTER_LINEAR)
         # cv2.drawContours(frame, [contour], -1, (0, 0, 0), cv2.FILLED)
 
-        warped_images.append((warped, pts, contour))
+        warped_images.append((warped, points, contour))
 
     return warped_images
 
@@ -140,7 +140,7 @@ def edge_process(image):
     area = 25 * 20
     if border is None or border.shape[1] < 300:
         return "0"
-    # print(border.shape, border.shape[1] / 20)
+
     index = 0
     for k in range(round(border.shape[1] / 20), border.shape[1], round(border.shape[1] / 20) * 2):
         # cv2.rectangle(image, (k, 0), (k + 24, border.shape[1]), (128, 128, 128), cv2.FILLED)
@@ -206,27 +206,27 @@ def marker_process(image, index):
         if _key in ID and counts[_key] > mm:
             mm = counts[_key]
             key, position = _key, POSITION[index]
-            # print(index, mats, POSITION[index])
+
         mats.append(mats.pop(0))
 
+    del mats
     return key, position, threshold
 
 def post_processing(frame, warped_images):
-    if len(warped_images) == 0:
+    if not warped_images:
         COUNT[0] += 1
         return frame
 
     result = frame.copy()
-    list_index = []
 
-    for i in range(len(warped_images)):
-        warped, points_warped, contour = warped_images[i]
+    for i, (warped, warped_points, contour) in enumerate(warped_images):
         key, position, threshold = marker_process(warped, i)
 
         # print(key, position)
         if key == "":
             continue
         if key in ID:
+            COUNT[1] += 1
             replace_image = ID[key]
             replace_image_size = replace_image.shape[0]
             if position == "LTRB":
@@ -240,21 +240,21 @@ def post_processing(frame, warped_images):
             else:
                 print("bug", key, position)
                 exit(0)
-            # cv2.imshow(str(i), replace_image)
             points = np.float32(
                 [[replace_image_size, 0],
                  [replace_image_size, replace_image_size],
                  [0, replace_image_size],
                  [0, 0]]
             )
-            M = cv2.getPerspectiveTransform(points, points_warped)
+            M = cv2.getPerspectiveTransform(points, warped_points)
             warped_image = cv2.warpPerspective(replace_image, M, (frame.shape[1], frame.shape[0]))
 
             cv2.drawContours(result, [contour], -1, (0, 0, 0), cv2.FILLED)
             result = cv2.bitwise_xor(result, warped_image, mask=None)
+        else:
+            COUNT[2] += 1
 
-    warped_images.clear()
-    list_index.clear()
+    del warped_images
     return result
 
 def find_frames(frame):
@@ -273,7 +273,8 @@ if __name__ == '__main__':
     # cv2.imwrite("as.jpg", image)
     # cv2.waitKey(0)
 
-    cap = cv2.VideoCapture("Data\\video2.mp4")
+    # cap = cv2.VideoCapture("Data\\video2.mp4")
+    cap = cv2.VideoCapture(0)
     frame_width, frame_height = int(cap.get(3)), int(cap.get(4))
     out = cv2.VideoWriter('outvideo2.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 10, (frame_width,
     frame_height))
@@ -286,6 +287,7 @@ if __name__ == '__main__':
         x = frame.copy()
         frame_count += 1
         time_start = time.clock()
+
         frames = find_frames(frame)
 
         time_elapsed = (time.clock() - time_start)
@@ -293,13 +295,14 @@ if __name__ == '__main__':
 
         result = np.hstack((x, frames))
         cv2.imshow("frame", result)
-        out.write(frames)
+        # out.write(frames)
         # if frame_count == 17:
         #     break
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     print(COUNT)
     # When everything done, release the capture
-    out.release()
+    # out.release()
     cap.release()
     cv2.destroyAllWindows()
+
